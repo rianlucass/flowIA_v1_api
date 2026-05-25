869261a0732b1a3246f94d95287d05265d8403be
Rian Lucas <rianlucas1638@gmail.com>
2026-05-25 16:31:15 -0300

    first commit

---
+++ src/main/java/com/br/rianlucas/flowia_api/FlowiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.rianlucas.flowia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/rianlucas/flowia_api/FlowiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rianlucas.flowia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
